c258fe5cc6a46e4f92dc7b8a14bcf70b6754060c
MADELAN <mariasilva708@gmail.com>
2021-09-30 21:08:19 -0300

    Summary and total function

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@
         let register= document.getElementById('btnregister');
         let divFail= document.getElementById('fail');
         let summary= document.getElementById('product-list');
+        let total= document.getElementById('total-summary');
 
         //SUMMARY
         let shirt= document.getElementById('event_shirt');
@@ -28,25 +29,58 @@
 
         calculate.addEventListener('click', calculate_amounts);
 
+
         function calculate_amounts(event) {
             event.preventDefault();
             if (gift.value ==='') {
                 alert("You need choice a gift");
                 gift.focus();
             } else {
+               
                 let ticketTicket= parseInt(ticket.value,10) || 0,
                     ticketVip= parseInt(vip.value, 10) || 0,
                     ticketCamping= parseInt(camping.value, 10) || 0,
                     amountShirt= parseInt(shirt.value, 10) || 0,
                     amountStickers=parseInt(stickers.value, 10) || 0;
-
-
-               let totalPay= 
+                let totalPay= 
                (ticketTicket*50)+(ticketVip*80)+(ticketCamping*120)+((amountShirt*15)*.93)+(amountStickers*5);
-               console.log(totalPay);
+               
+               let productList= [];
+
+               if (ticketTicket>=1) {
+                   productList.push(ticketTicket + 'Tickets');
+                }
+               if (ticketVip>=1) {
+                productList.push(ticketVip + 'VIP Tickets');
+                }
+               if (ticketCamping>=1) {
+                productList.push(ticketCamping + 'Camping Tickets');
+                }
+               if (amountShirt>=1) {
+                productList.push(amountShirt + 'Shirts');
+                }
+               if (amountStickers>=1) {
+                productList.push(amountStickers + 'Stickers');
+                }
+                
+                total.innerHTML='$' + totalPay.toFixed(2);
+                summary.innerHTML = '';
+
+                for (let i = 0; i < productList.length; i++) {
+                    summary.innerHTML = productList[i] += '<br/>';
+                    
+                }
+
+
+                summary.style.display='block';
+
+
+               
             }
+            
         }
 
+        
 
 
 
