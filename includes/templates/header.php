3d3df3a6e093f60e9c4b70d770f3e27e47aebbb9
MADELAN <mariasilva708@gmail.com>
2021-10-08 23:07:22 -0300

    PHP files

---
+++ includes/templates/header.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <LINK REL="StyleSheet" HREF="css/main.css" TYPE="text/css" media=screen>
+  
+
+  <meta name="theme-color" content="#fafafa">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/e65e6d4300.js" crossorigin="anonymous"></script> 
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+  
+  <!--Hero--> 
+  <header class="site-header">
+     <div class="hero">
+       <div class="content-header">
+         <nav class="social-media">
+           <a href="#"><i class="fab fa-facebook-f"></i></a>
+           <a href="#"><i class="fab fa-instagram"></i></a>
+           <a href="#"><i class="fab fa-twitter"></i></a>
+         </nav>
+         <div class="event-information">
+           <div class="event-dates">
+              <p class="date"><i class="far fa-calendar-alt"></i>27-Nov-2021</p>
+              <p class="city"><i class="fas fa-map-marker-alt"></i>Buenos Aires, Argentina</p>
+            </div>
+              <!-- Event information -->
+           <h1 class="site-name">Wish Festival</h1>
+           <p class="slogan">The best electronic festival that you <strong>Wish</strong></p>
+        </div>
+       </div>
+     </div>
+   </header>
+
+   <!--Menu-->
+   <div class="menu">
+     <div class="content clearfix">
+       <!--<div class="logo">
+         <a href="index.html">
+           <img src="" alt="Wish logo">
+         </a>
+       </div>-->
+       <div class="movile-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+         </div>
+
+        <nav class="main-menu clearfix">
+          <a class="program" href="registration.php">Get your Ticket NOW</a>
+          <a class="calendar" href="calendar.html">Calendar</a>
+          <a class="artist" href="artist.html">Artist</a>
+        </nav>
+      </div>
+   </div>
